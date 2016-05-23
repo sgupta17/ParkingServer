e1e73cc130b180715694fb956bc1e7e06f31207c
Fahim Choudhury <fchoudhury17@kentdenver.org>
2016-05-23 13:19:57 -0600

    Started Parking Css

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ handlebars = require('handlebars')
 
 var app = express();
 app.set('view engine', 'hbs');
+app.use(express.static('public'));
 
 app.get('/', function(req, res, next) {
     res.render('index', {title: "HELLO", content: "Hello, worasdfadsf!"});
@@ -10,10 +11,17 @@ app.get('/', function(req, res, next) {
 
 app.get('/radiodemo', function(req, res) {
     var nums = [];
-    for(var i = 0 ; i < 154; i++) {
+    for(var i = 1 ; i < 59; i++) {
         nums.push(i);
     }
-    res.render('radiodemo', {title: "Radio Demo", nums: nums});
+    res.render('radiodemo', {title: "Parking Diagram", nums: nums});
+});
+app.get('/radiodemo', function(req, res) {
+    var nums59 = [];
+    for(var i = 59 ; i < 123; i++) {
+        nums59.push(i);
+    }
+    res.render('radiodemo', {title: "Parking Diagram", nums59: nums59});
 });
 
 app.listen(3000);
