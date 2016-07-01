eef256cb8b1e5c433052055a7e71201d7f67a2ba
Shreyas Gupta <sgupta17@kentdenver.org>
2016-07-01 03:14:01 -0400

    Updated IP

--- public/update.js
+++ public/update.js
@@ -3,10 +3,10 @@ $(function() {
 })
 
 function prepare() {
-  $.get("http://10.0.1.6:3000/updatepos", function(data) {
+  $.get("http://159.203.200.152:3000/updatepos", function(data) {
     $('#posnum').html(data.pos);
     if (data.pos == "1st") {
-      window.location = "http://10.0.1.6:3000/parkingDiagram";
+      window.location = "http://159.203.200.152:3000/parkingDiagram";
     }
   })
   setTimeout(prepare, 1000);
